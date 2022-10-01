033885d98d6ebd93b5a152abec8d63ba77e7bde4
AvanMyers <smail1341@gmail.com>
2022-10-01 16:47:59 +0300

    work

---
+++ localMax/localMax/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace localMax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayElements = 30;
+            Random random = new Random();
+            int[] array = new int[arrayElements];
+            string localMaximum = "";
+
+            Console.Write("[ ");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                array[i] = random.Next(0, 50);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine("]");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                if (i == 0)
+                {
+                    if (array[0] < array[1]) 
+                    { 
+                    localMaximum += Convert.ToString(array[i] + " ");
+                    }
+                }
+                if (i < array.GetLength(0) - 1)
+                {
+                    if (array[i] <= array[i + 1] && array[i] <= array[i - 1])
+                    {
+                        localMaximum += Convert.ToString(array[i] + " ");
+                    }
+                } 
+                if (i == array.GetLength(0) - 1)
+                {
+                    if (array[i] < array[i - 1])
+                    {
+                        localMaximum += Convert.ToString(array[i] + " ");
+                    }
+                }
+            }
+            Console.WriteLine(localMaximum);
+            Console.ReadKey();
+        }
+    }
+}
